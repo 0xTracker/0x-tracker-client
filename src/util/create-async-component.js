18d67e29da3ad8dd82eaf932228f966571087728
Craig Bovis <craig@bovis.me.uk>
2018-12-15 17:57:06 +1100

    Use React.lazy for code splitting (#156)

--- src/util/create-async-component.js
+++ src/util/create-async-component.js
@@ -1,15 +1,16 @@
-import loadable from 'loadable-components';
 import React from 'react';
 
 import LoadingIndicator from '../components/loading-indicator';
 
-const createAsyncComponent = importer =>
-  loadable(importer, {
-    // eslint-disable-next-line react/display-name
-    render: ({ Component, loading, ownProps }) => {
-      if (loading) return <LoadingIndicator centered />;
-      return <Component {...ownProps} />;
-    },
-  });
+const createAsyncComponent = importer => {
+  const AsyncComponent = React.lazy(importer);
+  const ComponentLoader = ({ fallback, ...props }) => (
+    <React.Suspense fallback={fallback || <LoadingIndicator centered />}>
+      <AsyncComponent {...props} />
+    </React.Suspense>
+  );
+
+  return ComponentLoader;
+};
 
 export default createAsyncComponent;
