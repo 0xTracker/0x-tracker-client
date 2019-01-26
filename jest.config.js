fa2c0b9ce95a358482a3940de1c6175d5a0388cd
Craig Bovis <craig.bovis@inlight.com.au>
2019-01-26 12:38:14 +1100

    Remove babel-core bridge

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,7 @@ module.exports = {
     '^.+\\.(svg)$': '<rootDir>/config/jest/file-stub.js',
   },
   reporters: ['default'],
-  setupTestFrameworkScriptFile: '<rootDir>/config/jest/setup.js',
+  setupFilesAfterEnv: ['<rootDir>/config/jest/setup.js'],
   testEnvironment: 'jsdom',
   testMatch: ['**/__tests__/**/*.js', '**/*.test.js'],
 };
