f9af134d9a444614e04988629fb03fc9d55c2031
Craig Bovis <craig@bovis.me.uk>
2019-02-18 20:47:25 +1100

    Introduce and consume useApi hook (#301)
    * Introduce and consume useApi hook
    
    * Convert token volume metrics over to hooks

--- src/features/metrics/components/network-fees.js
+++ src/features/metrics/components/network-fees.js
@@ -1,95 +1,64 @@
-import _ from 'lodash';
 import { compose } from 'recompose';
 import { connect } from 'react-redux';
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { TIME_PERIOD } from '../../../constants';
-import { METRIC_TYPE } from '../constants';
-import { getNetworkMetrics } from '../selectors';
+import { getDisplayCurrency } from '../../currencies/selectors';
 import AsyncNetworkFeesChart from './async-network-fees-chart';
-import AutoReload from '../../../util/auto-reload';
 import LoadingIndicator from '../../../components/loading-indicator';
 import withConversionRate from '../../currencies/components/with-conversion-rate';
+import useNetworkMetrics from '../hooks/use-network-metrics';
 
-class FeesChart extends PureComponent {
-  componentDidMount() {
-    this.loadData();
-    AutoReload.addListener(this.loadData);
+const NetworkFees = ({
+  conversionRate,
+  displayCurrency,
+  period,
+  relayerId,
+}) => {
+  const networkMetrics = useNetworkMetrics({ period, relayerId });
+
+  if (networkMetrics.error) {
+    throw networkMetrics.error;
   }
 
-  componentDidUpdate(prevProps) {
-    const { period, relayerId } = this.props;
-
-    if (prevProps.period !== period || prevProps.relayerId !== relayerId) {
-      this.loadData();
-    }
+  if (networkMetrics.loading || conversionRate === undefined) {
+    return <LoadingIndicator centered />;
   }
 
-  componentWillUnmount() {
-    AutoReload.removeListener(this.loadData);
-  }
+  const data = networkMetrics.data.map(metric => ({
+    date: new Date(metric.date),
+    fees: metric.fees.ZRX,
+    localizedFees: metric.fees.USD * conversionRate,
+  }));
 
-  loadData = () => {
-    const { fetchMetrics, period, relayerId } = this.props;
+  return (
+    <AsyncNetworkFeesChart
+      {...{ data, localCurrency: displayCurrency, period }}
+    />
+  );
+};
 
-    fetchMetrics({
-      filter: { relayer: relayerId },
-      metricType: METRIC_TYPE.NETWORK,
-      period,
-    });
-  };
-
-  render() {
-    const { conversionRate, displayCurrency, metrics, period } = this.props;
-
-    if (_.some([metrics, conversionRate], _.isUndefined)) {
-      return <LoadingIndicator centered />;
-    }
-
-    const data = metrics.map(metric => ({
-      date: new Date(metric.date),
-      fees: metric.fees.ZRX,
-      localizedFees: metric.fees[displayCurrency],
-    }));
-
-    return <AsyncNetworkFeesChart {...{ data, displayCurrency, period }} />;
-  }
-}
-
-FeesChart.propTypes = {
+NetworkFees.propTypes = {
   conversionRate: PropTypes.number,
   displayCurrency: PropTypes.string.isRequired,
-  fetchMetrics: PropTypes.func.isRequired,
-  metrics: PropTypes.array,
   period: PropTypes.string,
   relayerId: PropTypes.string,
 };
 
-FeesChart.defaultProps = {
+NetworkFees.defaultProps = {
   conversionRate: undefined,
-  metrics: undefined,
   period: TIME_PERIOD.MONTH,
   relayerId: undefined,
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  metrics: getNetworkMetrics(state, {
-    period: ownProps.period || FeesChart.defaultProps.period,
-    relayer: ownProps.relayerId,
-  }),
-});
-
-const mapDispatchToProps = dispatch => ({
-  fetchMetrics: dispatch.metrics.fetch,
+const mapStateToProps = state => ({
+  displayCurrency: getDisplayCurrency(state),
 });
 
 const enhance = compose(
   withConversionRate,
-  connect(
-    mapStateToProps,
-    mapDispatchToProps,
-  ),
+  connect(mapStateToProps),
 );
 
-export default enhance(FeesChart);
+export default enhance(NetworkFees);
--- src/redux/models.js
+++ src/redux/models.js
@@ -1,4 +1,3 @@
-import metricsModel from '../features/metrics/models/metrics-model';
 import preferencesModel from '../features/preferences/models/preferences-model';
 import ratesModel from '../features/currencies/models/rates-model';
 import relayersModel from '../features/relayers/models/relayers-model';
@@ -6,7 +5,6 @@ import statsModel from '../features/stats/models/stats-model';
 import tokensModel from '../features/tokens/models/tokens-model';
 
 const models = {
-  metrics: metricsModel,
   preferences: preferencesModel,
   rates: ratesModel,
   relayers: relayersModel,
