33e1527ce671fd03421caf054ef845a19ebbaebe
Craig Bovis <craig.bovis@inlight.com.au>
2019-03-16 22:19:37 +1100

    Fix storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,9 +1,11 @@
-module.exports = (baseConfig, env, defaultConfig) => {
-  defaultConfig.module.rules.push({
-    enforce: 'pre',
-    loaders: [require.resolve('@storybook/addon-storysource/loader')],
-    test: /\.stories\.js?$/,
-  });
-
-  return defaultConfig;
+module.exports = {
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        loaders: [require.resolve('@storybook/addon-storysource/loader')],
+        test: /\.stories\.js?$/,
+      },
+    ],
+  },
 };
