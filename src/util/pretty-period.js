8175bf7378338e904dd882debc03e7c5af486e2f
Craig Bovis <cbovis@Craigs-MacBook-Air.local>
2019-05-13 09:38:30 +0800

    Introduce normalizePeriod util

--- src/util/pretty-period.js
+++ src/util/pretty-period.js
@@ -1,15 +1,7 @@
 import _ from 'lodash';
 
-import { TIME_PERIOD } from '../constants';
+import normalizePeriod from './normalize-period';
 
-const periodLabels = {
-  [TIME_PERIOD.DAY]: '24H',
-  [TIME_PERIOD.WEEK]: '7D',
-  [TIME_PERIOD.MONTH]: '1M',
-  [TIME_PERIOD.YEAR]: '1Y',
-  [TIME_PERIOD.ALL]: 'ALL',
-};
-
-const prettyPeriod = period => periodLabels[period] || _.toUpper(period);
+const prettyPeriod = period => _.toUpper(normalizePeriod(period));
 
 export default prettyPeriod;
