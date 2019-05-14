fbf660550238b1014439585217840380b6c2b3a4
Craig Bovis <craig@bovis.me.uk>
2019-05-14 18:47:51 +0800

    Handle unknown tokens on fill page (#441)

--- src/features/fills/components/asset-amount.js
+++ src/features/fills/components/asset-amount.js
@@ -1,9 +1,10 @@
+import _ from 'lodash';
 import React from 'react';
 
 import formatToken from '../../../util/format-token';
 
 const AssetAmount = ({ asset }) =>
-  asset.amount !== undefined &&
+  _.isString(asset.amount) &&
   asset.type !== 'erc-721' && <>{formatToken(asset.amount)} </>;
 
 export default AssetAmount;
