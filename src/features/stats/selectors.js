3fd1e129b83aaab7479f115614eec78e0874a8ef
Craig Bovis <cbovis@Craigs-MacBook-Air.local>
2019-05-15 11:51:01 +0800

    Remove redundant code from getTokensStats selector

--- src/features/stats/selectors.js
+++ src/features/stats/selectors.js
@@ -2,9 +2,6 @@ import _ from 'lodash';
 import { createSelector } from 'reselect';
 import objectHash from 'object-hash';
 
-import { getConversionRate, getDisplayCurrency } from '../currencies/selectors';
-import { BASE_CURRENCY } from '../currencies/constants';
-
 const getNetworkStats = createSelector(
   [(state, props) => props.period, state => _.get(state, 'stats.network')],
   (period, networkStats) => {
@@ -15,24 +12,12 @@ const getNetworkStats = createSelector(
 );
 
 const getTokensStats = (state, { period, relayerId }) => {
-  const conversionRate = getConversionRate(state);
-  const displayCurrency = getDisplayCurrency(state);
   const stats = _.get(
     state,
     `stats.tokens.${objectHash({ period, relayer: relayerId })}`,
   );
 
-  if (_.some([stats, conversionRate], _.isUndefined)) {
-    return undefined;
-  }
-
-  return _.map(stats, stat => ({
-    ...stat,
-    volume: {
-      ...stat.volume,
-      [displayCurrency]: stat.volume[BASE_CURRENCY] * conversionRate,
-    },
-  }));
+  return stats;
 };
 
 export { getNetworkStats, getTokensStats };
