1f2cc191d209b3f3d8516ffce9306860ec895797
Craig Bovis <craig@bovis.me.uk>
2019-08-12 11:54:07 +0700

    Ensure the formatToken util works with edge cases (#551)

--- src/util/format-token.js
+++ src/util/format-token.js
@@ -1,11 +1,31 @@
 import _ from 'lodash';
 
-const formatToken = amount => {
-  const formatted =
-    _.trimEnd(_.trimEnd(_.toNumber(amount.toString()).toFixed(6), '0'), '.') ||
-    amount;
+const trimTrailingZeroes = amount => _.trimEnd(_.trimEnd(amount, '0'), '.');
 
-  return formatted;
+const formatRecursively = (amount, precision) => {
+  if (_.isNil(amount)) {
+    return null;
+  }
+
+  const numericAmount = _.toNumber(amount);
+
+  if (Number.isNaN(numericAmount)) {
+    return null;
+  }
+
+  if (numericAmount === 0) {
+    return '0';
+  }
+
+  const formattedAmount = trimTrailingZeroes(numericAmount.toFixed(precision));
+
+  if (formattedAmount === '0') {
+    return formatRecursively(amount, precision + 1);
+  }
+
+  return formattedAmount;
 };
+
+const formatToken = amount => formatRecursively(amount, 6);
 
 export default formatToken;
