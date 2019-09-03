9f08e7a7d19fd5c843b1b77f1936e605901eba6f
Craig Bovis <craig@bovis.me.uk>
2019-09-03 12:31:32 +0200

    Remove breadcrumb from fills pages

--- src/features/fills/components/fills-page.js
+++ src/features/fills/components/fills-page.js
@@ -1,7 +1,6 @@
 import { Helmet } from 'react-helmet';
 import React from 'react';
 
-import { URL } from '../../../constants';
 import Card from '../../../components/card';
 import Fills from './fills';
 import PageLayout from '../../../components/page-layout';
@@ -11,10 +10,7 @@ const FillsPage = () => (
     <Helmet>
       <title>Browse Fills</title>
     </Helmet>
-    <PageLayout
-      breadcrumbItems={[{ title: 'Browse Fills', url: URL.FILLS }]}
-      title="Browse Fills"
-    >
+    <PageLayout title="Browse Fills">
       <Card fullHeight>
         <Fills />
       </Card>
