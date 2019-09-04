71f6d021f51c1b72f325ea5aeec18eb89964863c
Craig Bovis <craig@bovis.me.uk>
2019-09-04 18:26:03 +0200

    Decouple currency selector from redux

--- src/features/currencies/components/currency-selector.js
+++ src/features/currencies/components/currency-selector.js
@@ -1,51 +1,38 @@
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
+import React from 'react';
 import Select from 'react-select';
 
 import { CURRENCIES } from '../constants';
+import useDisplayCurrency from '../../preferences/hooks/use-display-currency';
 
 const OPTIONS = CURRENCIES.map(currency => ({
   label: `${currency.name} (${currency.symbol})`,
   value: currency.symbol,
 }));
 
-class CurrencySelector extends PureComponent {
-  constructor(props) {
-    super(props);
+const CurrencySelector = ({ className, name, onChange }) => {
+  const displayCurrency = useDisplayCurrency();
 
-    this.handleChange = this.handleChange.bind(this);
-  }
+  const handleChange = option => onChange(option.value);
 
-  handleChange(option) {
-    const { onChange } = this.props;
-
-    onChange(option.value);
-  }
-
-  render() {
-    const { className, defaultValue, name } = this.props;
-
-    return (
-      <Select
-        className={className}
-        classNamePrefix={className}
-        controlShouldRenderValue
-        defaultValue={OPTIONS.find(option => option.value === defaultValue)}
-        inputId={name}
-        isClearable={false}
-        isSearchable={false}
-        name={name}
-        onChange={this.handleChange}
-        options={OPTIONS}
-      />
-    );
-  }
-}
+  return (
+    <Select
+      className={className}
+      classNamePrefix={className}
+      controlShouldRenderValue
+      defaultValue={OPTIONS.find(option => option.value === displayCurrency)}
+      inputId={name}
+      isClearable={false}
+      isSearchable={false}
+      name={name}
+      onChange={handleChange}
+      options={OPTIONS}
+    />
+  );
+};
 
 CurrencySelector.propTypes = {
   className: PropTypes.string,
-  defaultValue: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
 };
@@ -54,8 +41,4 @@ CurrencySelector.defaultProps = {
   className: undefined,
 };
 
-const mapStateToProps = state => ({
-  defaultValue: state.preferences.currency,
-});
-
-export default connect(mapStateToProps)(CurrencySelector);
+export default CurrencySelector;
