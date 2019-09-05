2ea462e42ee1f5d8199caf4a7e60a009028badcf
Craig Bovis <craig@bovis.me.uk>
2019-09-05 14:54:42 +0200

    Add tests for buildApiUrl util

--- src/util/build-api-url.js
+++ src/util/build-api-url.js
@@ -4,15 +4,19 @@ import { flow, join, keys, map, omitBy } from 'lodash/fp';
 const buildApiUrl = (method, params, opts) => {
   const options = _.defaults({}, opts, { version: 1 });
   const endpoint = process.env.REACT_APP_API_ENDPOINT;
-  const querystring = flow([
+  const queryParams = flow([
     omitBy(value => value === undefined),
     keys,
     map(key => `${key}=${params[key]}`),
     join('&'),
   ])(params);
-  const url = `${endpoint}/v${options.version}/${method}?${querystring}`;
+  const query = queryParams.length > 0 ? `?${queryParams}` : '';
 
-  return url;
+  if (options.version === 1) {
+    return `${endpoint}/${method}${query}`;
+  }
+
+  return `${endpoint}/v${options.version}/${method}${query}`;
 };
 
 export default buildApiUrl;
