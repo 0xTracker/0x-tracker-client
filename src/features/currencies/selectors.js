3f51f08da14010b7f010e4adfafaca0f15ca515f
Craig Bovis <craig@bovis.me.uk>
2019-09-05 22:03:14 +0200

    Remove remaining redux models (#591)
    * Remove rates model from redux
    
    * Remove preferences model
    
    * Update README

--- src/features/currencies/selectors.js
+++
@@ -1,4 +0,0 @@
-const getDisplayCurrency = state => state.preferences.currency;
-const getRates = state => state.rates || undefined;
-
-export { getDisplayCurrency, getRates };
--- src/redux/models.js
+++
@@ -1,9 +0,0 @@
-import preferencesModel from '../features/preferences/models/preferences-model';
-import ratesModel from '../features/currencies/models/rates-model';
-
-const models = {
-  preferences: preferencesModel,
-  rates: ratesModel,
-};
-
-export default models;
