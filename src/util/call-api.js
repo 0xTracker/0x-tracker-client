1d6db843806aa71ad2683e67f6e512fc4eb8da60
Craig Bovis <craig@bovis.me.uk>
2019-09-16 19:04:46 +0100

    Ensure a reasonable API timeout is used

--- src/util/call-api.js
+++ src/util/call-api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
+import { API_CALL_TIMEOUT } from '../constants';
 import buildApiUrl from './build-api-url';
 
 const callApi = async (method, params, opts) => {
   const url = buildApiUrl(method, params, opts);
-  const response = await axios.get(url);
+  const response = await axios.get(url, { timeout: API_CALL_TIMEOUT });
 
   return response.data;
 };
