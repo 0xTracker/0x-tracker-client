05bf56cb0054448ce8227c6239d755bca4892890
Craig Bovis <craig@bovis.me.uk>
2019-09-26 19:37:49 +0100

    Remove redundant component

--- src/components/router.js
+++ src/components/router.js
@@ -2,13 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import ScrollToTop from './scroll-to-top';
-
-const Router = ({ children }) => (
-  <BrowserRouter>
-    <ScrollToTop>{children}</ScrollToTop>
-  </BrowserRouter>
-);
+const Router = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
 
 Router.propTypes = {
   children: PropTypes.node.isRequired,
