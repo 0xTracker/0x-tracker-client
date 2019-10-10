e680455e356d9ddd11233705a6232a3969551b77
Craig Bovis <craig@bovis.me.uk>
2019-10-10 19:45:06 +0100

    Remove network fees charts (#683)

--- src/features/metrics/components/async-network-fees-chart.js
+++
@@ -1,7 +0,0 @@
-import createAsyncComponent from '../../../util/create-async-component';
-
-const AsyncNetworkFeesChart = createAsyncComponent(() =>
-  import('./network-fees-chart'),
-);
-
-export default AsyncNetworkFeesChart;
