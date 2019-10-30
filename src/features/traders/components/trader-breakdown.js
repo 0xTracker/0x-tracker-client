b6bb3606eef1f92653e88f58a7df93f5731ac6e1
Craig Bovis <craig@bovis.me.uk>
2019-10-30 18:44:12 +0000

    Add real data to trader breakdown chart

--- src/features/traders/components/trader-breakdown.js
+++ src/features/traders/components/trader-breakdown.js
@@ -1,20 +1,36 @@
 import React from 'react';
 
 import AsyncTraderBreakdownChart from './async-trader-breakdown-chart';
+import LoadingIndicator from '../../../components/loading-indicator';
+import sharedPropTypes from '../../../prop-types';
+import useTraderStats from '../../stats/hooks/use-trader-stats';
 
-const TraderBreakdown = () => {
+const TraderBreakdown = ({ period }) => {
+  const [traderStats, loading] = useTraderStats({ period });
+
+  if (loading) {
+    return <LoadingIndicator centered />;
+  }
   const stats = [
     {
-      count: 328,
+      count: traderStats.makerCount,
       traderType: 'maker',
     },
     {
-      count: 542,
+      count: traderStats.takerCount,
       traderType: 'taker',
     },
   ];
 
   return <AsyncTraderBreakdownChart data={stats} />;
+};
+
+TraderBreakdown.propTypes = {
+  period: sharedPropTypes.timePeriod,
+};
+
+TraderBreakdown.defaultProps = {
+  period: undefined,
 };
 
 export default TraderBreakdown;
