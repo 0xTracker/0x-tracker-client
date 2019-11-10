25555d6ac63e901297af2625cc03cbcf2c95aa1a
Craig Bovis <craig@bovis.me.uk>
2019-11-10 13:58:24 +0000

    Redesign top tokens to be a list (#730)
    * Update top tokens to be a list rather than a chart
    
    * Handle long token names gracefully

--- src/features/tokens/components/async-top-tokens-chart.js
+++
@@ -1,7 +0,0 @@
-import createAsyncComponent from '../../../util/create-async-component';
-
-const AsyncTopTokensChart = createAsyncComponent(() =>
-  import('./top-tokens-chart'),
-);
-
-export default AsyncTopTokensChart;
--- src/features/tokens/components/token-link.js
+++ src/features/tokens/components/token-link.js
@@ -4,18 +4,22 @@ import React from 'react';
 import buildTokenUrl from '../util/build-token-url';
 import Link from '../../../components/link';
 
-const TokenLink = ({ address, children }) => (
-  <Link href={buildTokenUrl(address)}>{children}</Link>
+const TokenLink = ({ address, children, className }) => (
+  <Link className={className} href={buildTokenUrl(address)}>
+    {children}
+  </Link>
 );
 
 TokenLink.propTypes = {
   address: PropTypes.string,
   children: PropTypes.node,
+  className: PropTypes.string,
 };
 
 TokenLink.defaultProps = {
   address: undefined,
   children: undefined,
+  className: undefined,
 };
 
 export default TokenLink;
