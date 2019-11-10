11dd868c250e9f11a3995e1cb44c1190562aeb79
Craig Bovis <craig@bovis.me.uk>
2019-11-10 14:15:54 +0000

    Replace top relayers chart with a list

--- src/features/relayers/components/async-top-relayers-chart.js
+++
@@ -1,7 +0,0 @@
-import createAsyncComponent from '../../../util/create-async-component';
-
-const AsyncTopRelayersChart = createAsyncComponent(() =>
-  import('./top-relayers-chart'),
-);
-
-export default AsyncTopRelayersChart;
