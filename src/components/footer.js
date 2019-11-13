f23087b8e21a7feb63fc8f85bfb9ee0707d599c1
Craig Bovis <craig@bovis.me.uk>
2019-11-13 22:17:00 +0000

    Add additional links to footer (#733)
    * Add additional links to footer
    
    * Update tests for footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,98 +1,96 @@
 import { Col, Row } from 'reactstrap';
-import { rgba } from 'polished';
-import getYear from 'date-fns/getYear';
 import React from 'react';
 import styled from 'styled-components';
 
-import { DiscordIcon, GitHubIcon, TwitterIcon, MediumIcon } from './icons';
 import { media } from '../styles/util';
 import { colors } from '../styles/constants';
 import Container from './container';
 import Link from './link';
+import logoImage from '../assets/images/logo-grayscale.svg';
 
-const FooterIconLink = styled(Link)`
-  color: ${rgba(colors.white, 0.5)};
-  display: flex;
-  margin-left: 1rem;
+const Logo = styled.img`
+  height: 2.25rem;
+`;
 
-  &:first-child {
-    margin-left: 0;
-  }
+const NavHeading = styled.h2`
+  color: ${colors.white};
+  font-size: 1.1rem;
+  margin: 2rem 0 1rem 0;
+
+  ${media.greaterThan('md')`
+    margin: 0 0 1rem 0;
+  `}
+`;
+
+const Description = styled.p`
+  margin: 1rem 0 1.5rem;
+
+  ${media.greaterThan('md')`
+    margin: 1rem 0 0;
+  `}
+`;
+
+const NavLink = styled(Link)`
+  color: inherit;
+  display: block;
+  margin: 0 0 0.5rem;
 
   &:hover {
-    color: ${colors.white};
+    text-decoration: underline;
   }
-`;
-
-const FooterIcon = styled.svg`
-  height: 1.5rem;
-  vertical-align: middle;
-  width: 1.5rem;
-`;
-
-const FooterColumn = styled(Col).attrs({ md: 6, xs: 12 })`
-  align-items: center;
-  display: flex;
 `;
 
 const Footer = () => (
   <div
     css={`
-      background: ${colors.violet};
-      color: ${colors.white};
-      padding: 2rem;
+      background: #212035;
+      color: ${colors.santasGray};
+      padding: 2rem 1rem;
+
+      ${media.greaterThan('md')`
+        padding: 3rem 2rem;
+      `}
     `}
   >
     <Container>
       <Row>
-        <FooterColumn
-          css={`
-            justify-content: center;
-            margin-bottom: 1.5rem;
-
-            ${media.greaterThan('md')`
-              justify-content: flex-end;
-              margin-bottom: 0;
-              order: 2;
-            `}
-          `}
-        >
-          <FooterIconLink
-            href="https://github.com/0xtracker"
-            title="0x Tracker on GitHub"
-          >
-            <FooterIcon as={GitHubIcon} />
-          </FooterIconLink>
-          <FooterIconLink
-            href="https://discord.gg/tnV8hud"
-            title="Discuss on Discord"
-          >
-            <FooterIcon as={DiscordIcon} />
-          </FooterIconLink>
-          <FooterIconLink
-            href="https://twitter.com/0xTracker"
-            title="0x Tracker on Twitter"
-          >
-            <FooterIcon as={TwitterIcon} />
-          </FooterIconLink>
-          <FooterIconLink
-            href="https://medium.com/0x-tracker"
-            title="0x Tracker on Medium"
-          >
-            <FooterIcon as={MediumIcon} />
-          </FooterIconLink>
-        </FooterColumn>
-        <FooterColumn
-          css={`
-            justify-content: center;
-
-            ${media.greaterThan('md')`
-              justify-content: flex-start;
-            `}
-          `}
-        >
-          Copyright &copy; 0x Tracker {getYear(new Date())}
-        </FooterColumn>
+        <Col md={4} xs={12}>
+          <Logo alt="0x Tracker" src={logoImage} />
+          <Description>
+            The 0x protocol trade explorer, decentralised price index and news
+            aggregator.
+          </Description>
+        </Col>
+        <Col md={{ offset: 3, size: 2 }} sm={4} xs={6}>
+          <NavHeading>About</NavHeading>
+          <nav>
+            <NavLink href="https://docs.0xtracker.com/">Overview</NavLink>
+            <NavLink href="https://docs.0xtracker.com/faqs">FAQs</NavLink>
+            <NavLink href="https://medium.com/0x-tracker">Blog</NavLink>
+            <NavLink href="https://docs.0xtracker.com/#need-to-get-in-touch">
+              Contact
+            </NavLink>
+          </nav>
+        </Col>
+        <Col md={2} sm={4} xs={6}>
+          <NavHeading>Developers</NavHeading>
+          <nav>
+            <NavLink href="https://docs.0xtracker.com/api-reference/introduction">
+              API
+            </NavLink>
+            <NavLink href="https://docs.0xtracker.com/contributing">
+              Contributing
+            </NavLink>
+            <NavLink href="https://github.com/0xtracker">GitHub</NavLink>
+          </nav>
+        </Col>
+        <Col md={1} sm={4} xs={6}>
+          <NavHeading>Social</NavHeading>
+          <nav>
+            <NavLink href="https://twitter.com/0xTracker">Twitter</NavLink>
+            <NavLink href="https://discord.gg/tnV8hud">Discord</NavLink>
+          </nav>
+        </Col>
       </Row>
     </Container>
   </div>
--- src/components/footer.test.js
+++ src/components/footer.test.js
@@ -1,26 +1,10 @@
-import { getByText } from '@testing-library/react';
 import React from 'react';
-import timekeeper from 'timekeeper';
 
 import { renderWithRouter } from '../test-util/react';
 import Footer from './footer';
-
-beforeAll(() => {
-  timekeeper.freeze('2019-10-07');
-});
-
-afterAll(() => {
-  timekeeper.reset();
-});
 
 it('should render without props', () => {
   const { container } = renderWithRouter(<Footer />);
 
   expect(container.firstChild).toMatchSnapshot();
-});
-
-it('should render with the current year', () => {
-  const { container } = renderWithRouter(<Footer />);
-
-  getByText(container, 'Copyright © 0x Tracker 2019');
 });
