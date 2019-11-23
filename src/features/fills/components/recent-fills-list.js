db4a49d6b5a6763a81191b077e9b4cfeb900ca61
Craig Bovis <craig@bovis.me.uk>
2019-11-23 18:17:58 +0000

    Fix spacing in recent fills card (#748)
    * Fix spacing in recent fills table
    
    * Freeze time in tests

---
+++ src/features/fills/components/recent-fills-list.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import fillsPropTypes from '../prop-types';
+import RecentFillsItem from './recent-fills-item';
+
+const RecentFillsList = ({ fills }) => (
+  <div css="overflow-x: scroll">
+    {fills.map(fill => (
+      <RecentFillsItem fill={fill} key={fill.id} />
+    ))}
+  </div>
+);
+
+RecentFillsList.propTypes = {
+  fills: PropTypes.arrayOf(fillsPropTypes.partialFill).isRequired,
+};
+
+export default RecentFillsList;
