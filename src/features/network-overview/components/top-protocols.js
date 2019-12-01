77f7e7253c2ff936b4054c0ca0b8431a9806a26b
Craig Bovis <craig@bovis.me.uk>
2019-12-01 18:45:40 +0000

    Add footer to cards

--- src/features/network-overview/components/top-protocols.js
+++ src/features/network-overview/components/top-protocols.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 
+import { colors } from '../../../styles/constants';
+import { AsteriskIcon } from '../../../components/icons';
 import AsyncTopProtocolsChart from './async-top-protocols-chart';
+import sharedPropTypes from '../../../prop-types';
+import verbosePeriod from '../../../util/verbose-period';
 
-const TopProtocols = () => {
+const TopProtocolsFooter = styled.p`
+  align-items: center;
+  border-top: 1px solid ${colors.athensGray};
+  color: ${colors.stormGray};
+  display: flex;
+  font-size: 0.9rem;
+  justify-content: flex-end;
+  margin: 1rem 0 0;
+  padding-top: 0.5rem;
+`;
+
+const TopProtocols = ({ period }) => {
   const stats = [
     {
       protocolVersion: 2,
@@ -19,7 +35,19 @@ const TopProtocols = () => {
     { protocolVersion: 1, share: 1.6, tradeCount: 120, tradeVolume: 100000 },
   ];
 
-  return <AsyncTopProtocolsChart data={stats} />;
+  return (
+    <>
+      <AsyncTopProtocolsChart data={stats} />
+      <TopProtocolsFooter>
+        Top protocols by {verbosePeriod(period)} trade count
+        <AsteriskIcon css="margin-left: 0.5rem; opacity: 0.7;" size="12" />
+      </TopProtocolsFooter>
+    </>
+  );
+};
+
+TopProtocols.propTypes = {
+  period: sharedPropTypes.timePeriod.isRequired,
 };
 
 export default TopProtocols;
--- src/features/traders/components/trader-breakdown.js
+++ src/features/traders/components/trader-breakdown.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 
+import { colors } from '../../../styles/constants';
+import { AsteriskIcon } from '../../../components/icons';
 import AsyncTraderBreakdownChart from './async-trader-breakdown-chart';
 import LoadingIndicator from '../../../components/loading-indicator';
 import sharedPropTypes from '../../../prop-types';
 import useTraderStats from '../../stats/hooks/use-trader-stats';
+import verbosePeriod from '../../../util/verbose-period';
+
+const TraderBreakdownFooter = styled.p`
+  align-items: center;
+  border-top: 1px solid ${colors.athensGray};
+  color: ${colors.stormGray};
+  display: flex;
+  font-size: 0.9rem;
+  justify-content: flex-end;
+  margin: 1rem 0 0;
+  padding-top: 0.5rem;
+`;
 
 const TraderBreakdown = ({ period }) => {
   const [traderStats, loading] = useTraderStats({ period });
@@ -22,7 +37,15 @@ const TraderBreakdown = ({ period }) => {
     },
   ];
 
-  return <AsyncTraderBreakdownChart data={stats} />;
+  return (
+    <>
+      <AsyncTraderBreakdownChart data={stats} />
+      <TraderBreakdownFooter>
+        {verbosePeriod(period)} unique trader count by type
+        <AsteriskIcon css="margin-left: 0.5rem; opacity: 0.7;" size="12" />
+      </TraderBreakdownFooter>
+    </>
+  );
 };
 
 TraderBreakdown.propTypes = {
