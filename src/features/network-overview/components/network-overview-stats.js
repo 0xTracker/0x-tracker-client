38b1053cef6584fe0b6c6bb1ab3fa595f1527c54
Craig Bovis <craig@bovis.me.uk>
2019-12-01 18:42:38 +0000

    Fix stats

--- src/features/network-overview/components/network-overview-stats.js
+++ src/features/network-overview/components/network-overview-stats.js
@@ -6,22 +6,22 @@ import React from 'react';
 import { useCurrentBreakpoint } from '../../../responsive-utils';
 import ActiveTradersWidget from '../../traders/components/active-traders-widget';
 import FillVolumeWidget from '../../fills/components/fill-volume-widget';
+import sharedPropTypes from '../../../prop-types';
 import TradeCountWidget from '../../fills/components/trade-count-widget';
 import TradeVolumeWidget from '../../fills/components/trade-volume-widget';
 import useNetworkStats from '../../stats/hooks/use-network-stats';
 import useTraderStats from '../../stats/hooks/use-trader-stats';
 
 // Carousel gets loaded lazily because it relies on react-slick
-// const AsyncNetworkOverviewStatsCarousel = React.lazy(() =>
-//   import('./network-overview-stats-carousel'),
-// );
+const AsyncNetworkOverviewStatsCarousel = React.lazy(() =>
+  import('./network-overview-stats-carousel'),
+);
 
-const NetworkOverviewStats = ({ className }) => {
-  const [networkStats] = useNetworkStats();
-  const [traderStats] = useTraderStats();
+const NetworkOverviewStats = ({ className, period }) => {
+  const [networkStats] = useNetworkStats({ period });
+  const [traderStats] = useTraderStats({ period });
   const breakpoint = useCurrentBreakpoint();
 
-  const fillCount = _.get(networkStats, 'fillCount');
   const fillVolume = _.get(networkStats, 'fillVolume');
   const tradeCount = _.get(networkStats, 'tradeCount');
   const traderCount = _.get(traderStats, 'traderCount');
@@ -30,7 +30,7 @@ const NetworkOverviewStats = ({ className }) => {
   return breakpoint.greaterThan('md') ? (
     <Row className={className}>
       <Col lg={3} md={6}>
-        <FillVolumeWidget fillCount={fillCount} fillVolume={fillVolume} />
+        <FillVolumeWidget fillVolume={fillVolume} />
       </Col>
       <Col lg={3} md={6}>
         <TradeVolumeWidget volume={tradeVolume} />
@@ -43,20 +43,19 @@ const NetworkOverviewStats = ({ className }) => {
       </Col>
     </Row>
   ) : (
-    <div />
-    // <AsyncNetworkOverviewStatsCarousel
-    //   className={className}
-    //   fillCount={fillCount}
-    //   fillVolume={fillVolume}
-    //   tradeCount={tradeCount}
-    //   traderCount={traderCount}
-    //   tradeVolume={tradeVolume}
-    // />
+    <AsyncNetworkOverviewStatsCarousel
+      className={className}
+      fillVolume={fillVolume}
+      tradeCount={tradeCount}
+      traderCount={traderCount}
+      tradeVolume={tradeVolume}
+    />
   );
 };
 
 NetworkOverviewStats.propTypes = {
   className: PropTypes.string,
+  period: sharedPropTypes.timePeriod.isRequired,
 };
 
 NetworkOverviewStats.defaultProps = {
