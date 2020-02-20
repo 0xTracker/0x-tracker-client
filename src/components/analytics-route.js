0e1f7a09bc33d403d56b0e5342841c112dbd595e
Craig Bovis <craig@bovis.me.uk>
2020-02-20 10:17:59 -0500

    Remove analytics code

--- src/components/analytics-route.js
+++
@@ -1,8 +0,0 @@
-import { Route } from 'react-router-dom';
-import React from 'react';
-
-import AnalyticsTracker from './analytics-tracker';
-
-const AnalyticsRoute = () => <Route component={AnalyticsTracker} />;
-
-export default AnalyticsRoute;
