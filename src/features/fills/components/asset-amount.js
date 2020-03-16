d111fe57b34cfbd7e87a99b917eeb88211b9f3fe
Craig Bovis <craig@bovis.me.uk>
2020-03-16 14:33:17 -0500

    Fix linting errors

--- src/features/fills/components/asset-amount.js
+++ src/features/fills/components/asset-amount.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import PropTypes from 'prop-types';
 import React from 'react';
 
 import formatTokenAmount from '../../../util/format-token-amount';
@@ -6,5 +7,12 @@ import formatTokenAmount from '../../../util/format-token-amount';
 const AssetAmount = ({ asset }) =>
   _.isString(asset.amount) &&
   asset.type !== 'erc-721' && <>{formatTokenAmount(asset.amount)} </>;
+
+AssetAmount.propTypes = {
+  asset: PropTypes.shape({
+    amount: PropTypes.string,
+    type: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default AssetAmount;
