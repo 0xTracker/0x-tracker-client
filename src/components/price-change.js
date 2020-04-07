797380c6c5e4ea1740a634151fc30207cc7b2a11
Craig Bovis <craig@bovis.me.uk>
2020-04-06 19:23:06 -0500

    Improve some things around price change

--- src/components/price-change.js
+++ src/components/price-change.js
@@ -3,17 +3,37 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import { colors } from '../styles/constants';
-import { TrendingDownIcon, TrendingUpIcon } from './icons';
+import { TrendingDownIcon, TrendingFlatIcon, TrendingUpIcon } from './icons';
+
+const getColor = (value) => {
+  if (value > 0) {
+    return colors.fruitSalad;
+  }
+
+  if (value < 0) {
+    return colors.pomegranate;
+  }
+
+  return colors.stormGray;
+};
 
 const PriceChange = ({ children }) => (
   <span
     css={`
-      color: ${children > 0 ? colors.fruitSalad : colors.pomegranate};
+      color: ${getColor(children)};
       margin-left: 0.5rem;
       font-size: 1rem;
     `}
   >
     {numeral(children).format('0.[00]')}%
+    {children === 0 ? (
+      <TrendingFlatIcon
+        color={colors.stormGray}
+        css="margin: 0 0 0 0.25rem;"
+        height={20}
+        width={20}
+      />
+    ) : null}
     {children > 0 ? (
       <TrendingUpIcon
         color={colors.fruitSalad}
