4992001411eb04528b4145b541e5677131db329b
Craig Bovis <craig@bovis.me.uk>
2020-04-10 17:07:54 -0500

    Ensure token page is responsive

--- src/features/tokens/components/token-page-title.js
+++ src/features/tokens/components/token-page-title.js
@@ -1,42 +1,73 @@
 import _ from 'lodash';
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
 
 import { colors } from '../../../styles/constants';
+import { TIME_PERIOD } from '../../../constants';
+import Hidden from '../../../components/hidden';
 import LocalisedAmount from '../../currencies/components/localised-amount';
 import PriceChange from '../../../components/price-change';
+import SubTitle from '../../../components/sub-title';
 import TokenImage from './token-image';
+import Visible from '../../../components/visible';
 
-const TokenPageTitle = ({ token }) => (
-  <div css="align-items: center; display: flex;">
-    {_.has(token, 'imageUrl') ? (
+const PriceWrapper = styled.span`
+  align-items: center;
+  color: ${colors.stormGray};
+  display: flex;
+  margin-left: 2rem;
+`;
+
+const periodDescriptions = {
+  [TIME_PERIOD.DAY]: 'for the last 24 hours',
+  [TIME_PERIOD.WEEK]: 'for the past week',
+  [TIME_PERIOD.MONTH]: 'for the past month',
+  [TIME_PERIOD.YEAR]: 'for the past year',
+  [TIME_PERIOD.ALL]: 'for all time',
+};
+
+const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+`;
+
+const TokenPageTitle = ({ statsPeriod, token }) => (
+  <Wrapper>
+    {_.isString(token.imageUrl) && (
       <TokenImage
         css="margin-right: 0.75rem;"
         imageUrl={token.imageUrl}
         size="2rem"
       />
-    ) : null}
-    {_.has(token, 'name') ? token.name : `Token: ${token.address}`}
-    {_.has(token, 'symbol') ? ` (${token.symbol.toUpperCase()})` : null}
-    {token.price.last !== null ? (
-      <span css="align-items: center; color: gray; display: flex; margin-left: 2rem;">
-        <LocalisedAmount
-          amount={token.price.last}
-          css={`
-            color: ${colors.stormGray};
-          `}
-        />{' '}
-        {_.has(token, 'price.change') ? (
-          <PriceChange css="display: flex; align-items: center;">
-            {token.price.change}
-          </PriceChange>
-        ) : null}
-      </span>
-    ) : null}
-  </div>
+    )}
+    <span>
+      {_.isString(token.name) ? token.name : `Token: ${token.address}`}
+      {_.isString(token.symbol) && ` (${token.symbol.toUpperCase()})`}
+      <Hidden above="sm">
+        <SubTitle>{periodDescriptions[statsPeriod]}</SubTitle>
+      </Hidden>
+    </span>
+    {_.isFinite(token.price.last) && (
+      <Visible above="sm">
+        <PriceWrapper>
+          <LocalisedAmount amount={token.price.last} />
+          {_.isFinite(token.price.change) && (
+            <>
+              {' '}
+              <PriceChange css="display: flex; align-items: center;">
+                {token.price.change}
+              </PriceChange>
+            </>
+          )}
+        </PriceWrapper>
+      </Visible>
+    )}
+  </Wrapper>
 );
 
 TokenPageTitle.propTypes = {
+  statsPeriod: PropTypes.string.isRequired,
   token: PropTypes.shape({
     address: PropTypes.string.isRequired,
     imageUrl: PropTypes.string,
--- src/features/tokens/components/token-page.js
+++ src/features/tokens/components/token-page.js
@@ -1,129 +1,101 @@
 import _ from 'lodash';
 import { Helmet } from 'react-helmet';
-import PropTypes from 'prop-types';
+import { useHistory, useParams } from 'react-router';
+import { useSearchParam } from 'react-use';
 import React from 'react';
 
 import { TIME_PERIOD } from '../../../constants';
-import { useCurrentBreakpoint } from '../../../responsive-utils';
 import buildTokenUrl from '../util/build-token-url';
 import Card from '../../../components/card';
 import Fills from '../../fills/components/fills';
 import LoadingPage from '../../../components/loading-page';
-import MobileTimePeriodFilter from '../../../components/mobile-time-period-filter';
 import PageLayout from '../../../components/page-layout';
+import ResponsiveTimePeriodFilter from '../../../components/responsive-time-period-filter';
 import TabbedCard from '../../../components/tabbed-card';
-import TimePeriodFilter from '../../../components/time-period-filter';
 import TokenMetrics from '../../metrics/components/token-metrics';
 import TokenPageTitle from './token-page-title';
 import TokenStats from './token-stats';
 import useToken from '../hooks/use-token';
 
-const TokenPage = ({ history, location, match }) => {
-  const params = new URLSearchParams(location.search);
-  const statsPeriod = params.get('statsPeriod') || TIME_PERIOD.WEEK;
-  const breakpoint = useCurrentBreakpoint();
-  const { address: tokenAddress } = match.params;
-  const page = Number(params.get('page')) || 1;
-  const [token, loadingToken] = useToken(tokenAddress, { statsPeriod });
+const TokenPage = () => {
+  const history = useHistory();
+  const params = useParams();
+
+  const statsPeriod = useSearchParam('statsPeriod') || TIME_PERIOD.WEEK;
+  const page = Number(useSearchParam('page')) || 1;
+  const [token, loadingToken] = useToken(params.address, { statsPeriod });
 
   if (loadingToken) {
     return <LoadingPage />;
   }
 
-  const periodFilterProps = {
-    onChange: (newPeriod) => {
-      history.push(buildTokenUrl(token.address, { statsPeriod: newPeriod }));
-    },
-    value: statsPeriod,
-  };
-
-  const onPageChange = (newPage) => {
-    history.push(
-      buildTokenUrl(token.address, {
-        page: newPage,
-        statsPeriod,
-      }),
-    );
-  };
-
   return (
     <>
       <Helmet>
         <title>
-          {_.has(token, 'name') ? token.name : `Token: ${token.address}`}
+          {_.isString(token.name) ? token.name : `Token: ${token.address}`}
         </title>
       </Helmet>
       <PageLayout
         filter={
-          breakpoint.lessThan('sm') ? (
-            <MobileTimePeriodFilter {...periodFilterProps} />
-          ) : (
-            <TimePeriodFilter {...periodFilterProps} />
-          )
-        }
-        title={<TokenPageTitle token={token} />}
-      >
-        <TokenStats
-          css="margin-bottom: 2rem;"
-          period={statsPeriod}
-          token={token}
-        />
-        {token ? (
-          <TabbedCard
-            css="height: 360px; margin-bottom: 2rem;"
-            tabs={[
-              {
-                component: (
-                  <TokenMetrics
-                    period={statsPeriod}
-                    token={token}
-                    type="price.close"
-                  />
-                ),
-                title: 'Market Price',
-              },
-              {
-                component: <TokenMetrics period={statsPeriod} token={token} />,
-                title: 'Volume',
-              },
-              {
-                component: (
-                  <TokenMetrics
-                    period={statsPeriod}
-                    token={token}
-                    type="tradeCount"
-                  />
-                ),
-                title: 'Trades',
-              },
-            ]}
+          <ResponsiveTimePeriodFilter
+            onChange={(newPeriod) => {
+              history.push(
+                buildTokenUrl(token.address, { statsPeriod: newPeriod }),
+              );
+            }}
+            value={statsPeriod}
           />
-        ) : null}
+        }
+        title={<TokenPageTitle statsPeriod={statsPeriod} token={token} />}
+      >
+        <TokenStats period={statsPeriod} token={token} />
+        <TabbedCard
+          css="height: 360px; margin-bottom: 2rem;"
+          tabs={[
+            {
+              component: (
+                <TokenMetrics
+                  period={statsPeriod}
+                  token={token}
+                  type="price.close"
+                />
+              ),
+              title: 'Market Price',
+            },
+            {
+              component: <TokenMetrics period={statsPeriod} token={token} />,
+              title: 'Volume',
+            },
+            {
+              component: (
+                <TokenMetrics
+                  period={statsPeriod}
+                  token={token}
+                  type="tradeCount"
+                />
+              ),
+              title: 'Trades',
+            },
+          ]}
+        />
         <Card fullHeight>
           <Fills
-            filter={{ token: tokenAddress }}
-            onPageChange={onPageChange}
+            filter={{ token: token.address }}
+            onPageChange={(newPage) => {
+              history.push(
+                buildTokenUrl(token.address, {
+                  page: newPage,
+                  statsPeriod,
+                }),
+              );
+            }}
             page={page}
           />
         </Card>
       </PageLayout>
     </>
   );
-};
-
-TokenPage.propTypes = {
-  history: PropTypes.shape({
-    push: PropTypes.func.isRequired,
-  }).isRequired,
-  location: PropTypes.shape({
-    search: PropTypes.string.isRequired,
-  }).isRequired,
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      address: PropTypes.string.isRequired,
-    }).isRequired,
-    url: PropTypes.string.isRequired,
-  }).isRequired,
 };
 
 export default TokenPage;
