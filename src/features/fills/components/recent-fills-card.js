57c595d09280cbad6cb24fa2ebcee8e971b9ce67
Craig Bovis <craig@bovis.me.uk>
2020-04-10 17:35:46 -0500

    Add recent fills to token page

--- src/features/fills/components/recent-fills-card.js
+++ src/features/fills/components/recent-fills-card.js
@@ -10,7 +10,7 @@ import Link from '../../../components/link';
 import Pill from '../../../components/pill';
 import RecentFills from './recent-fills';
 
-const RecentFillsCard = ({ className }) => (
+const RecentFillsCard = ({ className, filter }) => (
   <Card className={className}>
     <CardHeader>
       <CardHeading>Recent Fills</CardHeading>
@@ -19,17 +19,23 @@ const RecentFillsCard = ({ className }) => (
       </Pill>
     </CardHeader>
     <CardBody padded>
-      <RecentFills />
+      <RecentFills filter={filter} />
     </CardBody>
   </Card>
 );
 
 RecentFillsCard.propTypes = {
   className: PropTypes.string,
+  filter: PropTypes.shape({
+    address: PropTypes.string,
+    relayer: PropTypes.string,
+    token: PropTypes.string,
+  }),
 };
 
 RecentFillsCard.defaultProps = {
   className: undefined,
+  filter: undefined,
 };
 
 export default RecentFillsCard;
--- src/features/tokens/components/token-page.js
+++ src/features/tokens/components/token-page.js
@@ -2,14 +2,16 @@ import _ from 'lodash';
 import { Helmet } from 'react-helmet';
 import { useHistory, useParams } from 'react-router';
 import { useSearchParam } from 'react-use';
+import { Col, Row } from 'reactstrap';
 import React from 'react';
+import styled from 'styled-components';
 
 import { TIME_PERIOD } from '../../../constants';
+import { media } from '../../../styles/util';
 import buildTokenUrl from '../util/build-token-url';
-import Card from '../../../components/card';
-import Fills from '../../fills/components/fills';
 import LoadingPage from '../../../components/loading-page';
 import PageLayout from '../../../components/page-layout';
+import RecentFillsCard from '../../fills/components/recent-fills-card';
 import ResponsiveTimePeriodFilter from '../../../components/responsive-time-period-filter';
 import TabbedCard from '../../../components/tabbed-card';
 import TokenMetrics from '../../metrics/components/token-metrics';
@@ -17,12 +19,28 @@ import TokenPageTitle from './token-page-title';
 import TokenStats from './token-stats';
 import useToken from '../hooks/use-token';
 
+const TokenPageColumn = styled(Col)`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1.25rem;
+
+  &:last-child {
+    margin-bottom: ${(props) => (props.lastRow ? '0' : '1.25rem')};
+  }
+
+  ${media.greaterThan('lg')`
+    margin-bottom: ${(props) => (props.lastRow ? '0' : '2rem')};
+
+    &:last-child {
+      margin-bottom: ${(props) => (props.lastRow ? '0' : '2rem')};
+    }
+  `}
+`;
+
 const TokenPage = () => {
   const history = useHistory();
   const params = useParams();
-
   const statsPeriod = useSearchParam('statsPeriod') || TIME_PERIOD.WEEK;
-  const page = Number(useSearchParam('page')) || 1;
   const [token, loadingToken] = useToken(params.address, { statsPeriod });
 
   if (loadingToken) {
@@ -79,20 +97,15 @@ const TokenPage = () => {
             },
           ]}
         />
-        <Card fullHeight>
-          <Fills
-            filter={{ token: token.address }}
-            onPageChange={(newPage) => {
-              history.push(
-                buildTokenUrl(token.address, {
-                  page: newPage,
-                  statsPeriod,
-                }),
-              );
-            }}
-            page={page}
-          />
-        </Card>
+        <Row>
+          <TokenPageColumn css="flex-grow: 1;" lastRow lg={7}>
+            <RecentFillsCard
+              css="flex-grow: 1;"
+              filter={{ token: token.address }}
+            />
+          </TokenPageColumn>
+          <TokenPageColumn css="flex-grow: 1;" lastRow lg={5} />
+        </Row>
       </PageLayout>
     </>
   );
