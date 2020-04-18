b1e8db36d0f9ba50a9a6b7fa46580405f0b6b468
Craig Bovis <craig@bovis.me.uk>
2020-04-17 22:10:10 -0500

    Revert to previous ZRX pricing logic

--- src/features/home/hooks/use-zrx-price.js
+++ src/features/home/hooks/use-zrx-price.js
@@ -1,17 +1,11 @@
 import _ from 'lodash';
 
-import { ZRX_TOKEN } from '../../../constants';
 import useApi from '../../../hooks/use-api';
 
 const useZrxPrice = (options) => {
   const { autoReload } = _.defaults({}, options, { autoReload: false });
-  const [zrxToken, loading] = useApi(`tokens/${ZRX_TOKEN.address}`, {
-    autoReload,
-  });
 
-  return zrxToken === undefined
-    ? [undefined, loading]
-    : [zrxToken.price, loading];
+  return useApi('zrx-price', { autoReload });
 };
 
 export default useZrxPrice;
