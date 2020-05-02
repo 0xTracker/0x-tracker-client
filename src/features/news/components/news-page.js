936cd970d17dc8e1f5567d77925252d1b9793bd8
Craig Bovis <craig@bovis.me.uk>
2020-05-02 12:06:20 -0500

    Use standardised components on news page

--- src/features/news/components/news-page.js
+++ src/features/news/components/news-page.js
@@ -1,11 +1,9 @@
 import _ from 'lodash';
-import { Col, Row } from 'reactstrap';
-import PropTypes from 'prop-types';
+import { useParams } from 'react-router';
 import React from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../../styles/constants';
-import { media } from '../../../styles/util';
 import { useCurrentBreakpoint } from '../../../responsive-utils';
 import { useMetadata } from '../../../hooks';
 import ArticleList from './article-list';
@@ -13,6 +11,9 @@ import ArticlesFilter from './articles-filter';
 import ArticlesProvider from './articles-provider';
 import Card from '../../../components/card';
 import CardBody from '../../../components/card-body';
+import CardGrid from '../../../components/card-grid';
+import CardGridCol from '../../../components/card-grid-col';
+import CardGridRow from '../../../components/card-grid-row';
 import CardHeader from '../../../components/card-header';
 import CardHeading from '../../../components/card-heading';
 import LoadingIndicator from '../../../components/loading-indicator';
@@ -37,19 +38,10 @@ const LoadMoreButton = styled.button`
   }
 `;
 
-const ArticlesColumn = styled(Col).attrs({ md: 8 })`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 2rem;
-
-  ${media.greaterThan('md')`
-    margin-bottom: 0;
-  `}
-`;
-
-const NewsPage = ({ match }) => {
+const NewsPage = () => {
   useMetadata({ title: '0x Ecosystem News & Updates' });
 
+  const params = useParams();
   const [sources, loadingSources] = useArticleSources();
   const breakpoint = useCurrentBreakpoint();
 
@@ -57,71 +49,65 @@ const NewsPage = ({ match }) => {
     return <LoadingPage />;
   }
 
-  const source = _.find(sources, { slug: match.params.source });
+  const source = _.find(sources, { slug: params.source });
 
   return (
     <PageLayout
       title={source ? `${source.name} News & Updates` : 'News & Updates'}
     >
-      <Row css="flex-grow: 1;">
-        <ArticlesColumn>
-          <Card autoHeight>
-            <CardBody>
-              <ArticlesProvider
-                limit={11}
-                source={source ? source.slug : undefined}
-              >
-                {({
-                  articles,
-                  canLoadMore,
-                  loadingInitial,
-                  loadingMore,
-                  loadMore,
-                }) =>
-                  loadingInitial ? (
-                    <LoadingIndicator centered />
-                  ) : (
-                    <>
-                      <ArticleList
-                        articles={articles}
-                        compact={breakpoint.equalTo('xs')}
-                        showImages={!breakpoint.equalTo('xs')}
-                      />
-                      {canLoadMore ? (
-                        <LoadMoreButton onClick={loadMore} type="button">
-                          {loadingMore ? (
-                            <LoadingIndicator size="small" type="cylon" />
-                          ) : (
-                            'Load More Stories'
-                          )}
-                        </LoadMoreButton>
-                      ) : null}
-                    </>
-                  )
-                }
-              </ArticlesProvider>
-            </CardBody>
-          </Card>
-        </ArticlesColumn>
-        <Col md={4}>
-          <Card>
-            <CardHeader>
-              <CardHeading>Filter by source</CardHeading>
-            </CardHeader>
-            <ArticlesFilter sources={sources} />
-          </Card>
-        </Col>
-      </Row>
+      <CardGrid>
+        <CardGridRow md={8}>
+          <CardGridCol>
+            <Card>
+              <CardBody>
+                <ArticlesProvider
+                  limit={11}
+                  source={source ? source.slug : undefined}
+                >
+                  {({
+                    articles,
+                    canLoadMore,
+                    loadingInitial,
+                    loadingMore,
+                    loadMore,
+                  }) =>
+                    loadingInitial ? (
+                      <LoadingIndicator centered />
+                    ) : (
+                      <>
+                        <ArticleList
+                          articles={articles}
+                          compact={breakpoint.equalTo('xs')}
+                          showImages={!breakpoint.equalTo('xs')}
+                        />
+                        {canLoadMore ? (
+                          <LoadMoreButton onClick={loadMore} type="button">
+                            {loadingMore ? (
+                              <LoadingIndicator size="small" type="cylon" />
+                            ) : (
+                              'Load More Stories'
+                            )}
+                          </LoadMoreButton>
+                        ) : null}
+                      </>
+                    )
+                  }
+                </ArticlesProvider>
+              </CardBody>
+            </Card>
+          </CardGridCol>
+          <CardGridCol md={4}>
+            <Card>
+              <CardHeader>
+                <CardHeading>Filter by source</CardHeading>
+              </CardHeader>
+              <ArticlesFilter sources={sources} />
+            </Card>
+          </CardGridCol>
+        </CardGridRow>
+      </CardGrid>
     </PageLayout>
   );
-};
-
-NewsPage.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      source: PropTypes.string,
-    }).isRequired,
-  }).isRequired,
 };
 
 export default NewsPage;
