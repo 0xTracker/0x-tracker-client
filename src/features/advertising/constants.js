9fd1f04c5cd2513c9ba14499cc4e7dfa056b6baa
Craig Bovis <craig@bovis.me.uk>
2020-05-08 18:48:05 -0500

    Fix fathom image

--- src/features/advertising/constants.js
+++ src/features/advertising/constants.js
@@ -1,8 +1,7 @@
 const FATHOM_ADVERT = {
   DESCRIPTION:
     'Privacy focused, GDPR compliant alternative to Google Analytics',
-  ICON_URL:
-    'https://d1hxm72v4n116f.cloudfront.net/2d67c732-ead1-4725-ad09-06d4bed5972f/app/cropped-fathom-analytics-180x180.png',
+  ICON_URL: 'https://resources.0xtracker.com/logos/fathom.png',
   TITLE: 'Fathom Analytics',
   URL: 'https://usefathom.com/ref/TFTVTT',
 };
