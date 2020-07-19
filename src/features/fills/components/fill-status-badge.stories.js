4800c8cac15086ec368c2893e850edea1da0af7d
Craig Bovis <craig@bovis.me.uk>
2020-07-19 16:41:44 +0200

    Removed unused files to fix build

--- src/features/fills/components/fill-status-badge.stories.js
+++
@@ -1,9 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import React from 'react';
-
-import FillStatusBadge from './fill-status-badge';
-
-storiesOf('Fills|FillStatusBadge', module)
-  .add('Pending', () => <FillStatusBadge>pending</FillStatusBadge>)
-  .add('Failed', () => <FillStatusBadge>failed</FillStatusBadge>)
-  .add('Successful', () => <FillStatusBadge>successful</FillStatusBadge>);
