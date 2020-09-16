f01f21c48b914c7ca2be3e8b30267e9cfc297122
Craig Bovis <craig@bovis.me.uk>
2020-09-16 13:45:24 +0200

    Add average trade size to network insights

---
+++ src/features/metrics/components/network-volume-chart.js
@@ -0,0 +1,160 @@
+import _ from 'lodash';
+import {
+  Bar,
+  CartesianGrid,
+  ComposedChart,
+  XAxis,
+  YAxis,
+  Line,
+  Tooltip,
+  Brush,
+} from 'recharts';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { COLORS } from '../../../styles/constants';
+import { formatAxisCurrency, formatAxisDate, formatAxisNumber } from '../util';
+import { getGranularityForMetrics } from '../../../util';
+import BrushableChartContainer from '../../../components/brushable-chart-container';
+import CardPlaceholder from '../../../components/card-placeholder';
+import NetworkVolumeTooltip from './network-volume-tooltip';
+import useDisplayCurrency from '../../preferences/hooks/use-display-currency';
+
+const NetworkVolumeChart = ({ data, period, type }) => {
+  const displayCurrency = useDisplayCurrency();
+
+  if (_.every(data, { [type]: 0 })) {
+    return (
+      <CardPlaceholder>
+        No data available for the selected period
+      </CardPlaceholder>
+    );
+  }
+
+  const granularity = getGranularityForMetrics(data);
+
+  return (
+    <BrushableChartContainer data={data}>
+      {({ brushIndexes, brushableData, handleBrushChange }) => (
+        <ComposedChart
+          data={brushableData}
+          margin={{ bottom: 0, left: 0, right: 0, top: 0 }}
+        >
+          <CartesianGrid
+            stroke={COLORS.NEUTRAL.MYSTIC_200}
+            strokeDasharray="8 8"
+            strokeOpacity={0.7}
+            vertical={false}
+          />
+          <Bar dataKey={type} fill={COLORS.NEUTRAL.MYSTIC_300} yAxisId={type} />
+          <Line
+            dataKey="avgTradeSize"
+            dot={false}
+            stroke={COLORS.ACCENT.ANZAC_500}
+            strokeWidth={2}
+            type="monotone"
+            yAxisId="avgTradeSize"
+          />
+          <XAxis
+            axisLine={{ stroke: COLORS.NEUTRAL.MYSTIC_200 }}
+            dataKey="date"
+            tick={{ fill: COLORS.NEUTRAL.MYSTIC_700, fontSize: '0.8em' }}
+            tickFormatter={(date) => formatAxisDate(date, period, granularity)}
+            tickLine={false}
+          />
+
+          <YAxis
+            allowDuplicatedCategory={false}
+            axisLine={false}
+            dataKey={type}
+            domain={['auto', 'auto']}
+            interval={0}
+            label={{
+              fill: COLORS.NEUTRAL.MYSTIC_500,
+              fillOpacity: 0.6,
+              fontSize: '0.7rem',
+              fontWeight: 500,
+              position: 'insideTopLeft',
+              value: type === 'tradeVolume' ? 'VOLUME' : 'TRADES',
+            }}
+            mirror
+            padding={{ top: 40 }}
+            scale="linear"
+            tick={{
+              fill: COLORS.PRIMARY.SCAMPI_800,
+              fontSize: '0.8em',
+              fontWeight: 500,
+            }}
+            tickFormatter={
+              type === 'tradeVolume'
+                ? (value) => formatAxisCurrency(value, displayCurrency)
+                : formatAxisNumber
+            }
+            tickLine={false}
+            yAxisId={type}
+          />
+          <YAxis
+            allowDuplicatedCategory={false}
+            axisLine={false}
+            dataKey="avgTradeSize"
+            label={{
+              fill: COLORS.ACCENT.ANZAC_500,
+              fontSize: '0.7rem',
+              fontWeight: 500,
+              position: 'insideTopRight',
+              value: 'AVG TRADE SIZE',
+            }}
+            mirror
+            orientation="right"
+            padding={{ top: 40 }}
+            scale="linear"
+            tick={{
+              fill: COLORS.PRIMARY.SCAMPI_800,
+              fontSize: '0.8em',
+              fontWeight: 500,
+            }}
+            tickFormatter={(value) =>
+              formatAxisCurrency(value, displayCurrency)
+            }
+            tickLine={false}
+            yAxisId="avgTradeSize"
+          />
+          <Tooltip
+            content={
+              <NetworkVolumeTooltip
+                currency={displayCurrency}
+                granularity={granularity}
+              />
+            }
+          />
+          <Brush
+            {...brushIndexes}
+            dataKey="date"
+            height={30}
+            onChange={handleBrushChange}
+            stroke={COLORS.NEUTRAL.MYSTIC_300}
+            tickFormatter={(date) => formatAxisDate(date, period, granularity)}
+          />
+        </ComposedChart>
+      )}
+    </BrushableChartContainer>
+  );
+};
+
+NetworkVolumeChart.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.instanceOf(Date).isRequired,
+      tradeCount: PropTypes.number.isRequired,
+      tradeVolume: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+  period: PropTypes.string.isRequired,
+  type: PropTypes.string,
+};
+
+NetworkVolumeChart.defaultProps = {
+  type: 'tradeVolume',
+};
+
+export default NetworkVolumeChart;
---
+++ src/features/metrics/components/network-volume.js
@@ -0,0 +1,76 @@
+import _ from 'lodash';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { TIME_PERIOD } from '../../../constants';
+import NetworkVolumeChart from './network-volume-chart';
+import LoadingIndicator from '../../../components/loading-indicator';
+import useConversionRate from '../../currencies/hooks/use-conversion-rate';
+import useDisplayCurrency from '../../preferences/hooks/use-display-currency';
+import useNetworkMetrics from '../hooks/use-network-metrics';
+
+const determineGranularity = (period) => {
+  if (_.isPlainObject(period)) {
+    return undefined; // Defer to API
+  }
+
+  if (period === TIME_PERIOD.ALL) {
+    return 'month';
+  }
+
+  if (period === TIME_PERIOD.YEAR) {
+    return 'week';
+  }
+
+  if (period === TIME_PERIOD.MONTH) {
+    return 'day';
+  }
+
+  return 'hour';
+};
+
+const NetworkVolume = ({ filters, period, type }) => {
+  const granularity = determineGranularity(period);
+  const [metrics, loading] = useNetworkMetrics(
+    { filters, granularity, period },
+    { autoReload: true },
+  );
+  const conversionRate = useConversionRate();
+  const displayCurrency = useDisplayCurrency();
+
+  if (loading || conversionRate === undefined) {
+    return <LoadingIndicator centered />;
+  }
+
+  const data = metrics.map((metric) => ({
+    avgTradeSize:
+      ((parseFloat(metric.tradeVolume) || 0) * conversionRate) /
+      metric.tradeCount,
+    date: new Date(metric.date),
+    tradeCount: metric.tradeCount,
+    tradeVolume: (parseFloat(metric.tradeVolume) || 0) * conversionRate,
+  }));
+
+  return (
+    <NetworkVolumeChart
+      currency={displayCurrency}
+      data={data}
+      period={period}
+      type={type}
+    />
+  );
+};
+
+NetworkVolume.propTypes = {
+  filters: PropTypes.object,
+  period: PropTypes.string,
+  type: PropTypes.string,
+};
+
+NetworkVolume.defaultProps = {
+  filters: undefined,
+  period: TIME_PERIOD.MONTH,
+  type: 'tradeVolume',
+};
+
+export default NetworkVolume;
